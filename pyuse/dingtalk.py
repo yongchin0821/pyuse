ec7d384293f6770eef4c370412a0a04e1518f57d
home-master <401219180@qq.com>
2022-04-18 23:49:01 +0800

    update

--- pyuse/dingtalk.py
+++ pyuse/dingtalk.py
@@ -6,8 +6,8 @@ import time
 import requests
 import base64
 import hashlib
-import urllib
 import hmac
+import urllib.parse
 
 
 class DingTalk:
@@ -36,37 +36,32 @@ class DingTalk:
 
         self.title = None
 
-    def set_config(self,
-                   access_token: str = None,
-                   key: str = None,
-                   app_secret: str = None,
-                   at_mobiles: list = None,
-                   is_at_all: bool = False):
+    def set_config(self, **kwargs):
         """
         set the config
-        :param access_token:
-        :param key:
-        :param app_secret:
-        :param at_mobiles:
-        :param is_at_all:
-        :return:
         """
-        self.url = f"https://oapi.dingtalk.com/robot/send?access_token={access_token}"
-        self.key = key
-        self.app_secret = app_secret
-        self.at_mobiles = at_mobiles
-        self.is_at_all = is_at_all
+        if "key" in kwargs:
+            self.key = kwargs["key"]
+        if "app_secret" in kwargs:
+            self.app_secret = kwargs["app_secret"]
+        if "at_mobiles" in kwargs:
+            self.at_mobiles = kwargs["at_mobiles"]
+        if "is_at_all" in kwargs:
+            self.is_at_all = kwargs["is_at_all"]
 
     def _get_stamp(self) -> dict:
         """
-        Counter sign
+        https://open.dingtalk.com/document/group/enterprise-created-chatbot
+        get sign method from https://open.dingtalk.com/document/robots/customize-robot-security-settings
         """
         timestamp = str(round(time.time() * 1000))
-        secret_enc = self.app_secret.encode('utf-8')
+        app_secret_enc = self.app_secret.encode('utf-8')
         string_to_sign = '{}\n{}'.format(timestamp, self.app_secret)
         string_to_sign_enc = string_to_sign.encode('utf-8')
-        hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+        hmac_code = hmac.new(app_secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
         sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+
+        # sign = base64.b64encode(hmac_code).decode('utf-8')
         return {"sign": sign, "timestamp": timestamp}
 
     def send(self, text):
@@ -89,7 +84,7 @@ class DingTalk:
         }
         if self.app_secret is not None:
             params = self._get_stamp()
-        resp = requests.post(url=self.url, json=data, params=params).json()
+        resp = requests.post(url=self.url, json=data, headers=params).json()
         if resp["errcode"] == 0:
             print(" 📧 dingTalk sent successfully!!")
         else:
@@ -99,16 +94,18 @@ class DingTalk:
 
 
 if __name__ == '__main__':
+    # ding = DingTalk(
+    #     access_token="690900b5ce6d5d10bb1218b8e64a4e2b55f96a6d116aaf50",
+    #     key="xxxx",
+    #     app_secret="xxxxx",
+    #     at_mobiles=[13700000000, 13800000000],
+    #     is_at_all=False,
+    # )
     ding = DingTalk(
-        access_token="690900b5ce6d5d10bb1218b8e64a4e2b55f96a6d116aaf50",
-        key="xxxx",
-        app_secret="xxxxx",
-        at_mobiles=[13700000000, 13800000000],
+        access_token="4fa5c5aa96a3eb982c545e6c37d2f40e31ced8f888b25fe8d7f039753b6b28ed",
+        app_secret="SEC72ec328612dce5b1f5bf3ced118cd75aa9dae66c7603913d53c7d890435efe3c",
+        at_mobiles=[15025463191],
         is_at_all=False,
     )
-    ding.set_config(access_token="690900b5ce6d5d10bb1218b8e64a4e2b55f96a6d116aaf50",
-                    key="xxxx",
-                    app_secret="xxxxx",
-                    at_mobiles=[13700000000, 13800000000],
-                    is_at_all=False, )
+    ding.send("#headrs")
     a = DingTalk()
